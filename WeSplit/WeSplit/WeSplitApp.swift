54862b0a45086f50759774c3d2eb8fb39cc254d4
payten <payten.tyler@gmail.com>
2022-04-22 09:24:58 -0600

    Initial Commit

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Payten O'Driscoll on 4/22/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
