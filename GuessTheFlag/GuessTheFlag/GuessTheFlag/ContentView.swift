00a4bbd9a0cf5e510302d43760b7a3d233deed39
payten <payten.tyler@gmail.com>
2022-08-29 12:04:59 -0600

    day 20

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Payten O'Driscoll on 8/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        
+       }
+}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Payten O'Driscoll on 8/17/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
