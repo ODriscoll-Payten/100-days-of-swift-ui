3e889280c7d01c761840d4cd4950ddd312709135
payten <payten.tyler@gmail.com>
2022-09-06 07:47:46 -0600

    day 23

---
+++ ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Payten O'Driscoll on 9/3/22.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
