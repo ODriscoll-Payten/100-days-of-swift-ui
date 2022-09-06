e9adabc8ef0ff206241ae75e0e2bfaf076f1b07f
payten <payten.tyler@gmail.com>
2022-09-06 11:59:45 -0600

    day 25
    rock paper scissors app

---
+++ RockPaperScissors/RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Payten O'Driscoll on 9/6/22.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
