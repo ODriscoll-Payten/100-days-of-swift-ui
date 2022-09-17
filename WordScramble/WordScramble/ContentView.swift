0e2ea5f62e3ee6a7469e191cbd33042f89933c79
payten <payten.tyler@gmail.com>
2022-09-17 09:59:18 -0600

    day 29
    started the WordScramble app

---
+++ WordScramble/WordScramble/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Payten O'Driscoll on 9/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+    
+   
+ }
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Payten O'Driscoll on 9/10/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
