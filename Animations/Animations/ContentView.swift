84cb7df8bc3ecfaf5d2bb7595a8f8a6f3df3a04d
payten <payten.tyler@gmail.com>
2022-09-21 18:42:59 -0600

    day 33

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -7,29 +7,49 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
     
-    @State private var animationAmount = 0.0
-    
-    var body: some View {
-        print(animationAmount)
-        
-        return VStack {
-            
-            Button("tap me") {
-                withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
-                    animationAmount += 360
-                }
-            }
-            .padding(50)
-            .background(.red)
-            .foregroundColor(.white)
-            .clipShape(Circle())
-            .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
-        }
-        .padding()
+    func body(content: Content) -> some View {
+        content
+            .rotationEffect(.degrees(amount), anchor: anchor)
+            .clipped()
     }
 }
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(active: CornerRotateModifier(amount: -90, anchor: .topLeading), identity: CornerRotateModifier(amount: 0, anchor: .topLeading))
+    }
+}
+
+struct ContentView: View {
+        @State private var enabled = false
+ 
+    
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .fill(.blue)
+                .frame(width: 200, height: 200)
+            
+            if enabled {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.pivot)
+            }
+        }
+        .onTapGesture {
+            withAnimation{
+                enabled.toggle()
+            }
+        }
+    }
+        
+    }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
